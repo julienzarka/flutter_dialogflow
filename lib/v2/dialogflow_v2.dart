3fbf27e5dc3d1188786dd9e3f9f7759f6e1c2605
Diego Dal Bosco <diegodalbo97@gmail.com>
2019-03-28 14:44:07 +0100

    Change detectIntent method to use DetectIntentRequest parameter

--- lib/v2/dialogflow_v2.dart
+++ lib/v2/dialogflow_v2.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter_dialogflow/v2/auth_google.dart';
+import 'package:flutter_dialogflow/v2/detect_intent_request.dart';
 import 'package:meta/meta.dart';
 
 class Intent {
@@ -108,19 +109,36 @@ class Dialogflow {
   final AuthGoogle authGoogle;
   final String language;
 
-  const Dialogflow({@required this.authGoogle, this.language="en"});
+  const Dialogflow({@required this.authGoogle, this.language = "en"});
 
   String _getUrl() {
     return "https://dialogflow.googleapis.com/v2/projects/${authGoogle.getProjectId}/agent/sessions/${authGoogle.getSessionId}:detectIntent";
   }
 
-  Future<AIResponse> detectIntent(String query) async {
-    var response = await authGoogle.post(_getUrl(),
-        headers: {
-          HttpHeaders.authorizationHeader: "Bearer ${authGoogle.getToken}"
-        },
-        body:
-            "{'queryInput':{'text':{'text':'$query','language_code':'$language'}}}");
+  Future<AIResponse> detectIntent(DetectIntentRequest params) async {
+    var response = await authGoogle.post(
+      _getUrl(),
+      headers: {
+        HttpHeaders.authorizationHeader: "Bearer ${authGoogle.getToken}"
+      },
+      body: json.encode(params.toJson()),
+    );
     return AIResponse(body: json.decode(response.body));
+  }
+
+  Future<AIResponse> detectIntentFromText(
+    String query, {
+    String languageCode,
+  }) async {
+    return detectIntent(
+      DetectIntentRequest(
+        queryInput: QueryInput(
+          text: TextInput(
+            text: query,
+            languageCode: languageCode != null ? languageCode : language,
+          ),
+        ),
+      ),
+    );
   }
 }
