e81971cfc27e847565a4c7feee1a7f6a28e7e09c
Diego Dal Bosco <diegodalbo97@gmail.com>
2019-03-28 15:44:15 +0100

    Remove deprecated language strings

--- lib/utils/language.dart
+++ lib/utils/language.dart
@@ -1,151 +1,91 @@
 class Language {
-  /* Chinese Cantonese */
+  /// Chinese Cantonese
   static String chineseCantonese = "zh-HK";
-  @Deprecated("Use chineseCantonese")
-  static String CHINESE_CANTONESE = chineseCantonese;
 
-  /* Chinese Simplified */
+  /// Chinese Simplified
   static String chineseSimplified = "zh-CN";
-  @Deprecated("Use chineseSimplified")
-  static String CHINESE_SIMPLIFIED = chineseSimplified;
 
-  /* Chinese Traditional */
+  /// Chinese Traditional
   static String chineseTraditional = "zh-TW";
-  @Deprecated("Use chineseTraditional")
-  static String CHINESE_TRADITIONAL = chineseTraditional;
 
-  /* Danish */
+  /// Danish
   static String danish = "da";
-  @Deprecated("Use danish")
-  static String DANISH = danish;
 
-  /* Dutch */
+  /// Dutch
   static String dutch = "nl";
-  @Deprecated("Use dutch")
-  static String DUTCH = dutch;
 
-  /* English */
+  /// English
   static String english = "en";
-  @Deprecated("Use english")
-  static String ENGLISH = english;
 
-  /* English Australian locale */
+  /// English Australian locale
   static String englishAustralianLocale = "en-AU";
-  @Deprecated("Use englishAustralianLocale")
-  static String ENGLISH_AUSTRALIAN_LOCALE = englishAustralianLocale;
 
-  /* English Canadian locale */
+  /// English Canadian locale
   static String englishCanadianLocale = "en-CA";
-  @Deprecated("Use englishCanadianLocale")
-  static String ENGLISH_CANADIAN_LOCALE = englishCanadianLocale;
 
-  /* English Great Britain locale */
+  /// English Great Britain locale
   static String englishGreatBritaniLocale = "en-GB";
-  @Deprecated("Use englishGreatBritaniLocale")
-  static String ENGLISH_GREAT_BRITANI_LOCALE = englishGreatBritaniLocale;
 
-  /* English Indian locale */
+  /// English Indian locale
   static String englishIndianLocale = "en-IN";
-  @Deprecated("Use englishIndianLocale")
-  static String ENGLISH_INDIAN_LOCALE = englishIndianLocale;
 
-  /* English US locale */
+  /// English US locale
   static String englishUsLocale = "en-US";
-  @Deprecated("Use englishUsLocale")
-  static String ENGLISH_US_LOCALE = englishUsLocale;
 
-  /* French */
+  /// French
   static String french = "fr";
-  @Deprecated("Use french")
-  static String FRENCH = french;
 
-  /* French Canadian locale */
+  /// French Canadian locale
   static String frenchCanadianLocale = "fr-CA";
-  @Deprecated("Use frenchCanadianLocale")
-  static String FRENCH_CANADIAN_LOCALE = frenchCanadianLocale;
 
-  /* France locale */
+  /// France locale
   static String frenchFranceLocale = "fr-FR";
-  @Deprecated("Use frenchFranceLocale")
-  static String FRENCH_FRANCE_LOCALE = frenchFranceLocale;
 
-  /* German */
+  /// German
   static String german = "de";
-  @Deprecated("Use german")
-  static String GERMAN = german;
 
-  /* Hindi */
+  /// Hindi
   static String hindi = "hi";
-  @Deprecated("Use hindi")
-  static String HINDI = hindi;
 
-  /* Indonesian */
+  /// Indonesian
   static String indonesian = "id";
-  @Deprecated("Use indonesian")
-  static String INDONESIAN = indonesian;
 
-  /* Italian */
+  /// Italian
   static String italian = "it";
-  @Deprecated("Use italian")
-  static String ITALIAN = italian;
 
-  /* Japanese */
+  /// Japanese
   static String japonese = "ja";
-  @Deprecated("Use japonese")
-  static String JAPANESE = japonese;
 
-  /* Korean */
+  /// Korean
   static String korean = "ko";
-  @Deprecated("Use korean")
-  static String KOREAN = korean;
 
-  /* Norwegian */
+  /// Norwegian
   static String norwegian = "no";
-  @Deprecated("Use norwegian")
-  static String NORWEGIAN = norwegian;
 
-  /* Portuguese */
+  /// Portuguese
   static String portuguese = "pt";
-  @Deprecated("Use portuguese")
-  static String PORTUGUESE = portuguese;
 
-  /* Portuguese Brazilian */
+  /// Portuguese Brazilian
   static String portugueseBrazilian = "pt-BR";
-  @Deprecated("Use portugueseBrazilian")
-  static String PORTUGUESE_BRAZILIAN = portugueseBrazilian;
 
-  /* Russian */
+  /// Russian
   static String russian = "ru";
-  @Deprecated("Use russian")
-  static String RUSSIAN = russian;
 
-  /* Spanish */
+  /// Spanish
   static String spanish = "es";
-  @Deprecated("Use spanish")
-  static String SPANISH = spanish;
 
-  /* Spanish Latin America locale */
+  /// Spanish Latin America locale
   static String spanishLatinAmerica = "es-419";
-  @Deprecated("Use spanishLatinAmerica")
-  static String SPANISH_LATIN_AMERICA = spanishLatinAmerica;
 
-  /* Spanish Spain */
+  /// Spanish Spain
   static String spanishSpain = "es-ES";
-  @Deprecated("Use spanishSpain")
-  static String SPANISH_SPAIN = spanishSpain;
 
-  /* Swedish */
+  /// Swedish
   static String swedish = "sv";
-  @Deprecated("Use swedish")
-  static String SWEDISH = swedish;
 
-  /* Thai */
+  /// Thai
   static String thai = "th";
-  @Deprecated("Use thai")
-  static String THAI = thai;
 
-  /* Ukranian */
+  /// Ukranian
   static String ukranian = "uk";
-  @Deprecated("Use ukranian")
-  static String UKRANIAN = ukranian;
 }
